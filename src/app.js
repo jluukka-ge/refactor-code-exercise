c4733a42e6fec977c003463b8f4119e989a55091
Joel Luukka <joel.luukka@geniem.com>
2021-12-15 11:19:20 +0200

    Further refactorings

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
+// Choose a suitable DB implementatio
 // var db = require('./dbModels')
 var db = require('./mockDbModels')
 
-// Store the entry and keywords to the database
-const storeEntry = async (userId, threadId, entry, keywords) => {
-  const newEntry = {
-    userId: userId,
-    threadId: threadId,
-    entry: entry,
-    keywords: keywords
-  }
-  return await db.models.EntryModel.create(newEntry)
-}
-
 // Process data and store to database
-const handleCreateEntry = async (res, requestData) => {
+const handleCreateEntry = (res, requestData) => {
   // Extract keywords from the new entry
   const allWords = requestData.entry.split(' ')
   let keywords = []
   for (const word of allWords) {
-    if (['C#', 'JavaScript', 'Python'].includes(word) && !keywords.includes(word)) {
+    if (['C#', 'JavaScript', 'Python'].includes(word)) {
       keywords.push(word)
     }
   }
 
-  const createdEntry = await storeEntry(requestData.userId, requestData.threadId, requestData.entry, keywords)
+  // Store to database
+  const newEntry = {
+    userId: requestData.userId,
+    threadId: requestData.threadId,
+    entry: requestData.entry,
+    keywords: keywords
+  }
+  return await db.models.EntryModel.create(newEntry)
 
   // Return the data of the newly created DB instance through HTTP response
   return res.json(createdEntry)
@@ -33,9 +30,9 @@ const handleCreateEntry = async (res, requestData) => {
 module.exports = app => app.post('/thread/:threadId', async (req, res) => {
   // Get data from the request
   const getDataFromRequest = req => ({
-    userId: req.user.id,
-    threadId: req.params.threadId,
-    entry: req.body.entry
+    userId: req.user.id,  // integer
+    threadId: req.params.threadId,  // integer
+    entry: req.body.entry  // string
   })
 
   const requestData = getDataFromRequest(req)
